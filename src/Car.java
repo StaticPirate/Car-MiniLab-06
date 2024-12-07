1abadb1fadcf89a4ef2407c88f2d1c5825498db0
StaticPirate <xanderd303@gmail.com>
2024-12-07 09:38:19 -0700

    commit

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Car {
+
+    // variables for the car objects
+    private int year;
+    private String make;
+    private String model;
+    private int miles;
+    private double fuelTankLevel;
+
+    public Car(int year, String make, String model, int miles, double fuel) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.miles = miles;
+        this.fuelTankLevel = fuel;
+    }
+
+    
+
+    public static void main(String[] args) {
+
+    }
 }
