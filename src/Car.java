b78f6abcba3e21e830c90deff052beee1ac0f709
StaticPirate <xanderd303@gmail.com>
2024-12-07 10:21:19 -0700

    getters and setters -the modifyFuelTank setter is not working

--- src/Car.java
+++ src/Car.java
@@ -5,17 +5,51 @@ public class Car {
     private String make;
     private String model;
     private int miles;
-    private double fuelTankLevel;
+    private double fuelTank;
 
-    public Car(int year, String make, String model, int miles, double fuel) {
+    public void Car(int year, String make, String model, int miles, double fuel) {
         this.year = year;
         this.make = make;
         this.model = model;
         this.miles = miles;
-        this.fuelTankLevel = fuel;
+        this.fuelTank = fuel;
     }
 
-    
+    public int getYear() {
+        return this.year;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getMiles() {
+        return this.miles;
+    }
+
+    public double getFuelTank() {
+        return this.fuelTank;
+    }
+
+    public void addMiles(int milesToAdd) {
+        this.miles = this.miles + milesToAdd;
+    }
+
+    public void modifyFuelTank(int miles) {
+
+    }
+
+    public boolean isFuelTankEmpty() {
+        if (this.fuelTank == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     public static void main(String[] args) {
 
